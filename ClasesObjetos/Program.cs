fae5efe3958871edc228334d7b278de406063df5
LeonelGirett <82616033+leonel1414@users.noreply.github.com>
2023-07-04 21:48:49 -0300

    Agregar archivos de proyecto.

---
+++ ClasesObjetos/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Puerta door = new Puerta(150,200,"rojo",false);
+door.MostrarEstado();
+class Puerta {
+    int ancho;
+    int alto;
+    string color;
+    bool abierto;
+
+    //Constructor
+    public Puerta(int ancho, int alto, string color, bool abierto) {
+        this.ancho = ancho;
+        this.alto = alto;
+        this.color = color;
+        this.abierto = abierto;
+    }
+
+   
+
+    public void MostrarEstado() 
+    {
+        Console.WriteLine("Ancho: " + ancho);
+        Console.WriteLine("Alto: " + alto);
+        Console.WriteLine("Color: " + color);
+        Console.WriteLine("Abierta ?: " + abierto);
+    }
+}
